39c37297bca4b11ba76491e37a3630aea13f3753
macs1756 <kindrativmaksum312@gmail.com>
2022-07-14 13:42:44 +0200

    part time

--- js/table.js
+++ js/table.js
@@ -67,3 +67,92 @@ wwBd = "";
 typeBd = "";
 colorBd ="";
 });
+
+
+//////////////////
+
+
+///window.setInterval(()=>{
+//	console.log("interval");
+//},2000);
+
+
+
+
+const les2In = document.querySelector("#les2");
+const les2btn = document.querySelector("#les2btn");
+
+
+if(!les2In.value){
+	les2btn.disabled = true;
+}
+
+les2In.addEventListener("input", function(){
+
+	if(les2In.value.length<5){
+		les2btn.disabled = true;
+	}else{
+		les2btn.disabled = false;
+	}
+})
+
+
+les2btn.addEventListener("click", ()=>{
+	console.log(`Дані передано: ${les2In.value}`);
+})
+
+
+
+
+//console.log(d.getDay());
+//console.log(d.getDate());
+//console.log(d.getMonth());
+//console.log(d.getFullYear());
+//console.log(d.getHours());
+//console.log(d.getMinutes());
+//console.log(d.getSeconds());
+
+
+let tHours = document.querySelector(".date__hours");
+let tMin = document.querySelector(".date__minuts");
+let tSecond = document.querySelector(".date__second");
+let wrDate = document.querySelector(".date");
+
+
+
+
+function timeOn(){
+const d = new Date();
+
+if(d.getHours()<10){
+	tHours.innerText = `0${d.getHours()}`;
+}else{
+	tHours.innerText = d.getHours();
+}
+
+
+if(d.getMinutes()<10){
+	tMin.innerText = `0${d.getMinutes()}`;
+}else{
+	tMin.innerText = d.getMinutes();
+}
+
+if(d.getSeconds()<10){
+	tSecond.innerText = `0${d.getSeconds()}`;
+}else{
+	tSecond.innerText = d.getSeconds();
+}
+
+
+
+
+
+
+
+
+
+}
+
+
+
+setInterval(timeOn , 1);
